9487fedad9d33a241259e5d861ba1cd2519bec14
stephane.duboze <stephane.duboze@ynov.com>
2023-01-25 16:18:24 +0100

    Button and API Call

--- map.js
+++ map.js
@@ -1,6 +1,24 @@
+/********** LOGO  *****************/
+var LeafIcon = L.Icon.extend({
+    options: {
+        //shadowUrl: 'leaf-shadow.png',
+        iconSize:     [50, 50],
+        // shadowSize:   [50, 64],
+        iconAnchor:   [25,50],
+        // shadowAnchor: [4, 62],
+        popupAnchor:  [0, -39]
+    }
+});
+var BasketBall = new LeafIcon({iconUrl: 'img/Basketball.png'}),
+    FootBall = new LeafIcon({iconUrl: 'img/Football.png'}),
+    Gymnase = new LeafIcon({iconUrl: 'img/Gymnase.png'}),
+    Natation = new LeafIcon({iconUrl: 'img/Natation.png'}),
 
+
+    Rugby = new LeafIcon({iconUrl: 'img/Rugby.png'});
+
+/********** MAP  *****************/
 var map = L.map('map').setView([45.7603831, 4.849664], 13);
-
 // source=https://leaflet-extras.github.io/leaflet-providers/preview/
 var OpenStreetMap_France = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
 	maxZoom: 20,
@@ -8,12 +26,57 @@ var OpenStreetMap_France = L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/
 });
 OpenStreetMap_France.addTo(map);
 
-fetch("sport.geojson").then(res => res.json()).then(data => {
-    // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data).addTo(map);
-    L.geoJson(data).bindPopup(
-    function(layer){
-        return layer.feature.properties.Nom ; 
-    }).addTo(map);
+var markers = L.layerGroup().addTo(map); // Create a layer group to store markers
 
+  fetch('http://localhost:5000/geo/').then(res => res.json()).then(data => {
+      // add GeoJSON layer to the map once the file is loaded
+      L.geoJson(data).addTo(markers);
+      L.geoJson(data).bindPopup(
+      function(layer){
+          return layer.feature.properties.Name ; 
+      }).addTo(markers); // Add new markers to the markers layer group
+  });
+
+function mapFetch(variableName) {
+    fetch('http://localhost:5000/geo/' + variableName)
+    .then(res => res.json())
+    .then(data => {
+        markers.clearLayers(); // Clear all markers before adding new ones
+        var selectedIcon;
+        if(variableName == "BasketBall"){
+            selectedIcon = BasketBall;
+        }
+        else if(variableName == "FootBall"){
+            selectedIcon = FootBall;
+        }
+        else if(variableName == "Gymnase"){
+            selectedIcon = Gymnase;
+        }
+        else if(variableName == "Natation"){
+            selectedIcon = Natation;
+        }
+        else if(variableName == "Rugby"){
+            selectedIcon = Rugby;
+        }
+        L.geoJson(data, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, {icon: selectedIcon});
+            },
+            onEachFeature: function (feature, layer) {
+                console.log(feature.properties);
+                console.error(feature.properties.Name);
+                layer.bindPopup(feature.properties.Name);
+            }
+        }).addTo(markers); // Add new markers to the markers layer group
+    });
+}
+
+
+
+
+
+/********** JQUERY  *****************/
+$(".btn-filtre").click(function(){
+    var maVariable = $(this).attr("id");
+    mapFetch(maVariable);
 });
